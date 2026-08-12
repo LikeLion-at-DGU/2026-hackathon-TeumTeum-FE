2e21a549d1967d7046febab598ad0d9ef5edd854
dogdevelophoyri <hyori7272@gmail.com>
2026-08-12 14:53:03 +0900

    style:  색상, radius, spacing 등 공통 디자인 값

---
+++ src/styles/theme.js
@@ -0,0 +1,44 @@
+const theme = {
+    colors: {
+        primary: "#32CD32",
+
+        gray: "#B2B2B2",
+        black: "#000000",
+
+        catagory: "#EFEFEF",
+        magazine_gray: "#F3F3F3",
+        
+        menubar: "#000000",
+        icon: "#B2B2B2",
+        menu_checked: "#32CD32",
+    },
+
+    fontsize: {
+        title: "24px",
+        subtitle: "12px",
+        catagory: "14px",
+        button: "14px",
+        history: "16px",
+
+        lg: "20px",
+        md: "16px",
+        sm: "13px",
+    },
+
+    fontWeight: {
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+    },
+
+    borderRadius: {
+
+        sm: "16px",
+        md: "20px",
+        xl: "27px",
+    }
+
+}
+
+export default theme
