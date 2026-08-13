d20e2ca6e3b4dd197e4a787dcffae46a505161f6
dogdevelophoyri <hyori7272@gmail.com>
2026-08-13 13:25:23 +0900

    feat/#12-온보딩화면구현

---
+++ src/components/common/ProgressBar.jsx
@@ -0,0 +1,7 @@
+import * as S from "./ProgressBar.styled"
+
+const ProgressBar = () => {
+    return (
+        <S.ProgressBar />
+    )
+}
--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,5 +1,18 @@
+import OnboardingHeader from "../../components/onboarding/OnboardingHeader.jsx"
+import OptionSelect from "../../components/onboarding/OptionSelect.jsx";
+
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  return (
+    <>
+    <OnboardingHeader 
+      currentStep={2}
+      totalStep={3}
+    />
+    <OptionSelect>
+      
+    </OptionSelect>
+    </>
+  )
 };
 
 export default Onboarding;
