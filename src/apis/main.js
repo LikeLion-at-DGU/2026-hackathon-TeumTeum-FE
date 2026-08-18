e4bc5acb6bd90d9605a5d265a23ad00136c57c79
dogdevelophoyri <hyori7272@gmail.com>
2026-08-18 21:58:54 +0900

    동건선배님 이것 좀 봐주세요.. ㅠㅠ

--- src/apis/main.js
+++ src/apis/main.js
@@ -1,6 +1,16 @@
-import client from "./client"
+import client from "./client";
+import { getGuestUuid } from "../utils/guestUuid";
 
 export const getMain = async () => {
-    const response = await client.get("/main");
+    const guestUuid = getGuestUuid();
+
+    const response = await client.get("/main", {
+        params: {
+        guest_uuid: guestUuid,
+        },
+    });
+    
+    localStorage.getItem("guest_uuid")
+
     return response.data;
-}
+};
