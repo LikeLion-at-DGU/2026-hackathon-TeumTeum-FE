c38079a13595123bca0c2244e0ffa7fc6115a666
dogdevelophoyri <hyori7272@gmail.com>
2026-08-19 17:19:11 +0900

    매거진 get API연결

--- src/pages/Magazine/Magazine.styled.js
+++ src/pages/Magazine/Magazine.styled.js
@@ -6,6 +6,17 @@ export const Container = styled.div`
   width: 100%;
   box-sizing: border-box;
 
-  padding: 120px 28px 20px 28px;
+  padding: 100px 28px 20px 28px;
   gap: 25px;
 `;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #e6f2e3;
+  border-radius: 30px;
+  padding: 20px;
+  gap: 10px;
+`
+export const Text = styled.span`
+`
