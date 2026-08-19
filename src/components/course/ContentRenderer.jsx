5916b8acb5b997c8b78a952a61c6d685d5ad44b3
onlyvalue <sarahkim2917@gmail.com>
2026-08-19 15:13:24 +0900

    feat: 코스 생성 및 실행화면 api 연결

--- src/components/course/ContentRenderer.jsx
+++ src/components/course/ContentRenderer.jsx
@@ -1,16 +1,28 @@
 import VideoContent from "./contents/VideoContent";
 import AiBriefContent from "./contents/AiBriefContent";
+import BreathingContent from "./contents/BreathingContent";
+import StretchingContent from "./contents/StretchingContent";
+import ReflectionContent from "./contents/ReflectionContent";
 
 const ContentRenderer = ({ content }) => {
-  switch(content.content_type) {
+  switch (content.content_type) {
     case "youtube":
         return <VideoContent content={content} />;
 
     case "article":
         return <AiBriefContent content={content} />;
-    
+
+    case "audio_guide":
+        return <BreathingContent content={content} />;
+
+    case "stretch_guide":
+        return <StretchingContent content={content} />;
+
+    case "reflection":
+        return <ReflectionContent content={content} />;
+
     default:
-        return null;
+        return <p>지원하지 않는 콘텐츠입니다.</p>;
   }
 };
 
