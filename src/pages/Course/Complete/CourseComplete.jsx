3634cee570fce7582c967a6aade9bea64ab3d422
onlyvalue <sarahkim2917@gmail.com>
2026-08-20 13:57:03 +0900

    feat: breathinigcontent 세부 실행화면 api연동

--- src/pages/Course/Complete/CourseComplete.jsx
+++ src/pages/Course/Complete/CourseComplete.jsx
@@ -1,9 +1,25 @@
 import Header from "../../../components/layout/Header";
 import * as S from "./CourseComplete.styled"
 import Button from "../../../components/common/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const CourseComplete = () => {
+    const {state} = useLocation();
+
+    const completedExecution = state?.completedExecution;
+    const targetMinutes = state?.targetMinutes ?? 0;
+
+    const usedSeconds = completedExecution?.used_seconds ?? 0;
+    const usedMinutes = Math.floor(usedSeconds / 60);
+    const remainingSeconds = usedSeconds % 60;
+
+    const formattedUsedTime = remainingSeconds > 0
+        ? `${usedMinutes}분 ${remainingSeconds}초`
+        : `${usedMinutes}분`;
+
+    const todayCourses = completedExecution?.today_courses ?? 0;
+    const completedContents = completedExecution?.completed_contents ?? 0;
+    
     return (
         <>
         <Header />
@@ -25,7 +41,7 @@ const CourseComplete = () => {
                     </S.Confetti>
                 </S.IconWrapper>
                 <S.ContentWrapper>
-                    <S.Title>30분을 보냈어요</S.Title>
+                    <S.Title>{targetMinutes}분을 보냈어요</S.Title>
                     <S.Description>오늘의 작은 틈이 나를 위한 시간이 되었어요</S.Description>
                 </S.ContentWrapper>
             </S.CompleteSummary>
@@ -33,21 +49,24 @@ const CourseComplete = () => {
                 <S.RecordTitle>오늘의 웰니스 기록</S.RecordTitle>
                 <S.InfoWrapper>
                     <S.Info>
-                        <S.Times>30분</S.Times>
+                        <S.Times>{formattedUsedTime}</S.Times>
                         <S.Label>총 활동 시간</S.Label>
                     </S.Info>
                     <S.Info>
-                        <S.Times>3개</S.Times>
+                        <S.Times>{completedContents}개</S.Times>
                         <S.Label>완료한 활동</S.Label>
                     </S.Info>
                     <S.Info>
-                        <S.Times>1회</S.Times>
+                        <S.Times>{todayCourses}회</S.Times>
                         <S.Label>오늘의 코스</S.Label>
                     </S.Info>
                 </S.InfoWrapper>
-                <S.SaveMessage>오늘의 기록이 저장되었어요</S.SaveMessage>
+                    <S.SaveMessage>오늘의 기록이 저장되었어요</S.SaveMessage>
                 <S.ButtonWrapper>
-                <Button variant="secondary">다음 틈도 보낼래요</Button>
+                    <Link to="/home">
+                        <Button variant="secondary">다음 틈도 보낼래요</Button>
+                    </Link>
+                
                 <Link to={`/history`}>
                     <Button variant="primary">기록 보기</Button>
                 </Link>
@@ -58,4 +77,4 @@ const CourseComplete = () => {
     )
 }
 
-export default CourseComplete
+export default CourseComplete;
